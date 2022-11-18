fae9913aef6f6baf87c8d6140ca2d36cf30f2684
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-18 15:02:34 -0600

    Validate input

--- assets/script/index.js
+++ assets/script/index.js
@@ -14,13 +14,42 @@ function select(selector, parent = document) {
     return parent.querySelector(selector);
 }
 
+const btn = select('.set');
+const output = select('.output p');
+const inputField = select('#input');
+const timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+
+// Function to validate input
+function isValid(input) {
+    if (Number.isInteger(input)) {
+        return true;
+    }
+
+    return false;
+}
+
+function validateInput() {
+
+    let time = select('.input').value.trim();
+    let valid = true;
+
+    if (time.length === 0) {
+        output.innerText = 'choose a time';
+        valid = false;
+    } else if (!timeRegex.test(time)) {
+        output.innerText = 'Invalid time';
+        valid = false;
+    }
+}
+
+onEvent('click', btn, function() {
+    validateInput();
+});
+
 
 // Selections
-
 const time = select('.time');
 const date = new Date();
-
-const day = 1000 * 60 * 60 * 24;
 
 const displayTime = setInterval(() => {
   const second = date.getSeconds().toString().padStart(2, '0');;
@@ -30,3 +59,6 @@ const displayTime = setInterval(() => {
   time.innerHTML = `${hour} : ${minute} : ${second}`;
 
 }, 1000);
+
+
+
