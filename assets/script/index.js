0662375dd328d745d516931542607fa515fe47ca
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-18 23:16:29 -0600

    Add audio

--- assets/script/index.js
+++ assets/script/index.js
@@ -16,8 +16,8 @@ function select(selector, parent = document) {
 
 const btn = select('.set');
 const output = select('.output p');
-const inputField = select('#input');
 const timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+const bell = select('.fa-bell');
 
 // Function to validate input
 function isValid(input) {
@@ -39,26 +39,60 @@ function validateInput() {
     } else if (!timeRegex.test(time)) {
         output.innerText = 'Invalid time';
         valid = false;
+    } else {
+        output.innerText = time;
+        bell.classList = 'is-visible';
     }
 }
 
+// Selections
+const time = select('.time');
+
+function displayTime() {
+    const date = new Date();
+
+    const second = date.getSeconds();
+    const minute = date.getMinutes();
+    const hour = date.getHours();
+
+    const hours = hour.toString().padStart(2, '0');
+    const minutes = minute.toString().padStart(2, '0');
+    const seconds = second.toString().padStart(2, '0');
+
+    const clockTime = `${hours}: ${minutes}: ${seconds}`;
+    time.innerHTML = clockTime;
+   
+    let setTime = select('.input').value.trim();
+    if(setTime === `${hours}:${minutes}`) {
+        alarmTone.play();
+        audioStop();
+        time.style.color = '#b41a15'
+    } 
+} 
+setInterval(displayTime, 1000);
+
+
+// To get set time display
+const submit = (event) => {
+    event.preventDefault();
+}
+onEvent("submit", document.forms[0], submit);
+
+
+// Set Alarm
+const alarmTone = new Audio('./assets/audio/alarm-sound.wav');
+alarmTone.type = 'audio/wav';
+
+
+function audioStop(){
+    alarmTone.muted = false;        
+    setTimeout(() => {
+        alarmTone.pause();
+    }, 2000);
+}
+
+// Alarm ringtone 
 onEvent('click', btn, function() {
     validateInput();
 });
-
-
-// Selections
-const time = select('.time');
-const date = new Date();
-
-const displayTime = setInterval(() => {
-  const second = date.getSeconds().toString().padStart(2, '0');;
-  const minute = date.getMinutes().toString().padStart(2, '0');;
-  const hour = date.getHours().toString().padStart(2, '0');;
-
-  time.innerHTML = `${hour} : ${minute} : ${second}`;
-
-}, 1000);
-
-
 
